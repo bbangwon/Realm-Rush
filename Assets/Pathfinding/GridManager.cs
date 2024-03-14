4d2775523f71032f52ab65de0d34f7a131fd8821
bbangwon <bbangwon.chung@gmail.com>
2024-03-15 01:08:15 +0900

    순수 C# 클래스

---
+++ Assets/Pathfinding/GridManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]
+    Node node;
+
+    private void Start()
+    {
+        Debug.Log(node.coordinates);
+        Debug.Log(node.isWalkable);
+    }
+}
